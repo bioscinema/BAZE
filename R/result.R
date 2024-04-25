c7b457d5a4551a14e408c649e20b4bd2201cb6ba
Ruitao Liu <rxl761@case.edu>
2024-04-25 10:43:20 -0400

    fix bugs

--- R/result.R
+++ R/result.R
@@ -92,9 +92,8 @@ result <-
       ss = 50
       al=-15
       au=0
-      a0=-4
       # a0 = seq(from = al, to = au, by = (au-al) / ss)
-      a <- rep(a0,p)
+      a <- rep(a,p)
 
       result <- gibbsgamma(nburnin, niter, p, nop, Y, X, N, a, Q, n, tau, nu, omega, seed, TRUE, stand, TRUE)
       return(result)
